5ff7052e2be9013ca3432816eaed0ff56d044423
luciD <sosalnet@yandex.ru>
2023-01-25 02:38:16 +0300

    main/aboutProject

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AboutProject from "./AboutProject/AboutProject";
 import Promo from "./Promo/Promo";
 
 function Main() {
   return (
     <main className="main">
       <Promo />
+      <AboutProject />
     </main>
   );
 }
