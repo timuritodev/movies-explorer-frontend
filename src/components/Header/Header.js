4cf71f343e1e3690e9c46f40dd74ac927a21e432
luciD <sosalnet@yandex.ru>
2023-01-24 15:37:55 +0300

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navigation } from "../Navigation/Navigation";
+import NavAuth from "../NavAuth/NavAuth";
+
+function Header() {
+  return (
+    <header className="header">
+      <Link className="header__logo" to="/"></Link>
+      <NavAuth />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/NavAuth/NavAuth.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavAuth() {
+  return (
+    <nav className="nav-auth">
+      <ul className="nav-auth__list">
+        <Link className="nav-auth__link" to="/signup">Регистрация</Link>
+        <Link className="nav-auth__link nav-auth__link_log" to="/sigin">Войти</Link>
+      </ul>
+    </nav>
+);
+}
+
+export default NavAuth;
