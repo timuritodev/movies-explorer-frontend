14ac6f66b46b62ccfa9041262c3b051c8e252797
luciD <sosalnet@yandex.ru>
2023-01-25 13:02:55 +0300

    main/techs

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AboutProject from "./AboutProject/AboutProject";
 import Promo from "./Promo/Promo";
+import Techs from "./Techs/Techs";
 
 function Main() {
   return (
     <main className="main">
       <Promo />
       <AboutProject />
+      <Techs />
     </main>
   );
 }
---
+++ src/components/Main/Techs/Techs.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Techs() {
+  return (
+    <section className="techs">
+      <h2 className="section__title section__title_plus">Технологии</h2>
+      <p className="techs__subtitle">7 технологий</p>
+      <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <ul className="techs__lists">
+        <li className="techs__list">HTML</li>
+        <li className="techs__list">CSS</li>
+        <li className="techs__list">JS</li>
+        <li className="techs__list">React</li>
+        <li className="techs__list">Git</li>
+        <li className="techs__list">Express.js</li>
+        <li className="techs__list">mongoDB</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Techs;
