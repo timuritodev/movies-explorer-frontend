195cefe0bdcab01c0e2cfea74b4e7b35a9ef62a6
luciD <sosalnet@yandex.ru>
2023-01-25 23:41:09 +0300

    main/portfolio

---
+++ src/components/Main/Portfolio/Portfolio.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__block">
+        <li className="portfolio__links"><Link className="portfolio__link" to="https://github.com/sosalnet/how-to-learn">Статичный сайт</Link></li>
+        <li className="portfolio__links"><Link className="portfolio__link" to="https://github.com/sosalnet/russian-travel">Адаптивный сайт</Link></li>
+        <li className="portfolio__links"><Link className="portfolio__link" to="https://github.com/sosalnet/react-mesto-api-full">Одностраничное приложение</Link></li>
+      </ul>
+    </section>
+  );
+}
+
+export default Portfolio;
