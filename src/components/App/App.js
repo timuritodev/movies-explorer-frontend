4b1cd2f6579bb8f031ca355b6c9911d9b699ba07
luciD <sosalnet@yandex.ru>
2023-01-28 00:20:29 +0300

    register - profile - notfound

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,18 @@ import Footer from '../Footer/Footer';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies'
 import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Profile from '../Profile/Profile';
+import NotFound from '../NotFound/NotFound';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Login />
+      {/* <Header /> */}
+      <NotFound />
+      {/* <Profile /> */}
+      {/* <Register />
+      <Login /> */}
       {/* <Movies /> */}
       {/* <SavedMovies /> */}
       {/* <Routes>
---
+++ src/components/Register/Register.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Register() {
+  return (
+    <section className="login">
+      <div className="login__block">
+      <Link className="header__logo" to="/"></Link>
+      <h1 className="login__title">Добро пожаловать!</h1>
+      <form className="login-form" name="register">
+        <label className="login-form__label">Имя</label>
+        <input className="login-form__input" name="name" id="name"  type="text" placeholder="Имя"></input>
+        <span className="login-form__error"></span>
+        <label className="login-form__label">E-mail</label>
+        <input className="login-form__input" name="email" id="email"  type="email" placeholder="Email"></input>
+        <span className="login-form__error"></span>
+        <label className="login-form__label">Пароль</label>
+        <input className="login-form__input" name="password" id="password"  type="password" placeholder="Пароль"></input>
+        <span className="login-form__error"></span>
+        <button className="login-form__button" type="submit">Зарегистрироваться</button>
+        <p className="login-form__text">Уже зарегистрированы?<Link className="login-form__link" to="/">{" "} Войти</Link></p>
+      </form>
+      </div>
+    </section>
+  );
+}
+
+export default Register;
